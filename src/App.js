7904eb4b83bfd0543d9359a96beaf8a6cbdac44b
Lamriver123 <ngoclamhttv@gmail.com>
2025-08-18 09:26:39 +0700

    Thong tin ca nhan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nguyễn Hữu Ngọc Lam - 22110362
         </p>
         <a
           className="App-link"
